c3fa9c2cd67c0499d4bafc6754a2de3592d63476
wntitus <wesn.titus@gmail.com>
2018-07-25 22:05:35 -0400

    completed logic for dynamically filling in questions and minor styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,26 @@ $(document).ready(function(){
     var gameTimer = 120;
     var gameBody = $(".gameBody");
     var timerBox = $("<div class = 'timer'>");
-    var questions = 
-        [{
+    var questions = {
+        questionOne : {
             question : "Who is Spongebob's best friend?",
+            choices: ["Patrick", "Gary", "Arthur", "Boof"],
             answer : "Patrick"
-        },{
+        },
+        questionTwo : {
             question : "What was Tommy's (from Rugrats) last name?",
+            choices: ["Cucumbers", "Pickles", "Eggplants", "Beets"],
             answer : "Pickles"
-        }];
+        },
+        questionThree : {
+            question: "How many bears are there in We Bare Bears?",
+            choices: ["Four", "Six", "Three", "Ten"],
+            answer : "Three"
+        }
+        
+    } 
+    
+    objSize = Object.keys(questions).length;
     
 
     function timeDown() {
@@ -19,14 +31,27 @@ $(document).ready(function(){
     }
 
     function fillQuestions() {
-        for (i = 0; i < questions.length; i++) {
-            var questionP = $("<p>");
-            questionP.append(questions[i].question);
-            gameBody.append(questionP);
+        for (var prop in questions) { 
+            for (i = objSize-1; i < objSize; i++) {
+                var obj = questions[prop];
+                var objChoices = obj.choices;
+                var qDiv = $("<div class = 'question'>");
+                $(".gameBody").append(qDiv);
+                qDiv.append(obj.question);
+                var formElem = $("<form>");
+                qDiv.append(formElem);
+                for (j = 0; j < objChoices.length; j++) {
+                    console.log(objChoices[j]);
+                    var inputElem = $("<input type = 'radio' name = 'answer' id = 'radio-choice" + j + "'><label for = 'radio-choice" + j + "'>");
+                    formElem.append(inputElem);
+                    inputElem.append(objChoices[j]);
+                }
+                
+            }
         }
-    }    
+    }
     
-
+    
 
 
 
